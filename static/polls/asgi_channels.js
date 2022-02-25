563be581895b6fba1e8dce6b580a321fa011d1f7
abrhm11 <100416339+abrhm11@users.noreply.github.com>
2022-02-25 19:20:51 +0200

    Add files via upload

---
+++ static/polls/asgi_channels.js
@@ -0,0 +1,9 @@
+let url = 'ws://${window.location.host}/ws./socket-server/';
+
+const chatSocket = new WebSocket(url);
+
+chatSocket.onmessage = function (e){
+    let data = JSON.parse(e.data)
+    console.log('Data:', data)
+}
+
